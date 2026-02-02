2218b4e422a517335a3bf9b734aa0e7467f39e16
Carlos <carlos8189.mk@gmail.com>
2026-02-01 23:01:04 -0300

    initial commit: remove assets and initial cod

---
+++ app/(private)/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { Geist } from 'next/font/google';
+import '../globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Agendamentos - Barbearia',
+  description: 'Agendamentos - Barbearia',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={`${geistSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
